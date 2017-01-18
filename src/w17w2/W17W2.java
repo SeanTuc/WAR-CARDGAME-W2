7c6ac974b96974c9f6b481da0cc0b05057bd87d3
Sean <Sean@10.250.13.96>
2017-01-18 12:24:37 -0500

    Initial Submission

---
+++ src/w17w2/W17W2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w17w2;
+
+/**
+ *
+ * @author Sean
+ */
+public class W17W2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.printf("hello Git hub");
+        // TODO code application logic here
+    }
+    
+}
